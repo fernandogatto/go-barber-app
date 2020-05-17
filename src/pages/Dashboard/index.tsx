c222910628edcacf75363e0cf6649fe35657b8e4
fernandogatto <fernandogatto17@gmail.com>
2020-05-16 23:17:44 -0300

    Initializating dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+
+import { useAuth } from '../../hooks/AuthContext';
 
 const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
     <>
       <View>
-        <Text>Dashboard</Text>
+        <Text style={{textAlign: 'center', color: '#fff'}}>Dashboard</Text>
+        <Button title="Sair" onPress={signOut} />
       </View>
     </>
   );
